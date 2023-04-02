2f66d5929fce9d382c5e34ab0f3aa6dbf60fac1b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-02 18:30:44 +0200

    _new feature

---
+++ plugins/_Translate.js
@@ -0,0 +1,34 @@
+                                 const { Amarok } = require('../lib');
+                                 const {
+                                 translate
+                                 } = require('@vitalets/google-translate-api');
+               const defaultLang = 'en'
+
+     Amarok.addCDM(
+      {
+      pattern: "tr",
+      fromMe: true,
+      desc: "Chat Gpt Chat feture",
+      dontAddCommandList: true,
+      type: "misc",
+      },
+   async (conn, match) => {
+
+      // if (!match || !m.quoted.text)  await conn.sendMessage(`📌 *Example:*\n\n*tr* <lang> [text]\n*tr* ar Hello World`)
+
+      let args = match.split(' ')
+      let lang = args[0]
+      let text = args.slice(1).join(' ')
+      if ((args[0] || '').length !== 2) {
+         lang = defaultLang
+         text = args.join(' ')
+      }
+      if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+      let result = await translate(text, {
+         to: lang,
+         autoCorrect: true
+      }).catch(_ => null)
+      conn.sendMessage(result.text)
+
+   })
