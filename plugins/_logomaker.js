c9ec02135cee34a5eb3eebc29c3677c7e42a110f
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-06 16:43:34 +0200

    Create _logomaker.js

---
+++ plugins/_logomaker.js
@@ -0,0 +1,10 @@
+  const Amarok = require('../lib/events');
+  const { getBuffer } require('../lib');
+  
+
+
+                           Amarok.addCMD({ pattern: "glitchtext", isOwner: false, desc: "logo maker", type: "logomaker", }, async(conn, match, text) => { if(!conn) await conn.reply("_need name_");
+                                                        var webimage = await getBuffer(`https://api.ibeng.tech/api/photooxy/glitch?text=${conn}Revita&text2=cans&apikey=tamvan`); await conn.sendMessage(webimage, {}, "image"
+             );
+      }
+ );
