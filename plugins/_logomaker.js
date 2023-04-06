3cc767053ceaf2330085013f6e85d3763ab7683c
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-06 21:41:06 +0200

    ♻🌲

--- plugins/_logomaker.js
+++ plugins/_logomaker.js
@@ -3,8 +3,8 @@
   
 
 
-                           Amarok.addCMD({ pattern: "glitchtext", isOwner: false, desc: "logo maker", type: "logomaker", }, async(conn, match, text) => { if(!conn) await conn.reply("_need name_");
-                                                        var webimage = await getBuffer(`https://api.ibeng.tech/api/photooxy/glitch?text=${conn}Revita&text2=cans&apikey=tamvan`); await conn.sendMessage(webimage, {}, "image"
+                           Amarok.addCMD({ pattern: "glitchtext", isOwner: false, desc: "logo maker", type: "logomaker", }, async(message, match, text) => { match = match [1] || message.reply_msg  if(!match) return await message.reply("_need name_");
+                                                        var webimage = await getBuffer(`https://api.ibeng.tech/api/photooxy/glitch?text=${match}Revita&text2=cans&apikey=tamvan`); await message.sendMessage(webimage, {}, "image"
              );
       }
  );
