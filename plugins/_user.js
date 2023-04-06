45c9f483909a6361f06d083a710e1105db8531c6
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-06 22:24:45 +0200

    Update and rename _logomaker.js to _user.js

---
+++ plugins/_user.js
@@ -0,0 +1,19 @@
+         const Amarok = require('../lib");
+       
+
+        Amarok.addCMD({
+                pattern: "block",
+                isOwner: true,
+                type: "user",
+           },
+           async(conn,match) => {
+          
+         const chatld =
+             conn.mention[0] ||
+             conn.reply_message.chatld ||
+             (!conn.isGroup && conn.chatld)
+             if (!chatld) return await conn.sendMessage("_mention user to block_");
+             await conn.sendMessage("_Blocked Successfully_");
+             await conn.Block(chatld)
+    }
+ )
