053fd9b35daee0decb80272e6b3718d4c6987714
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-07 18:05:50 +0200

    Create _eval.js

---
+++ plugins/_eval.js
@@ -0,0 +1,33 @@
+
+
+const {
+  fromBuffer,
+  getBuffer,
+  getJson,
+  parseJid,
+  runtime,
+  mUrl,
+  getRandom,
+  checkWAVersion,
+  fetchText,
+  mYtld,
+  webp2mp4File,
+  Amarok,
+} = require("../lib/");
+const util = require("util");
+const config = require("../config");
+
+Amarok.addCMD({pattern:'eval', on: 'text', isOwner: true,desc :'Runs a server code'}, async (conn, match, m, client) => {
+  if (match.startsWith(">")) {
+  if(!match) return await message.sendMessage("𝐔𝚴𝐅𝚯𝚪𝚻𝐔𝚴𝚫𝚻𝚵𝐋𝐘 𝚻𝚮𝚰𝐒 𝐂𝐃𝚳 𝚰𝐒 𝐅𝚯𝚪 𝚯𝐖𝚴𝚵𝚪");
+    //const m = message;
+    try {
+      let evaled = await eval(`${match.replace(">", "")}`);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      await conn.reply(evaled);
+    } catch (err) {
+      await conn.reply(util.format(err));
+    }
+  }
+});
+
