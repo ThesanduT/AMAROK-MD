372e2b99f684b29f670140096c8b237d943d91be
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-10 13:33:21 +0200

    Create List.js

---
+++ plugins/List.js
@@ -0,0 +1,50 @@
+const Amarok = require('./lib/events')
+const Config = require('../config')
+ const config = require('../config')
+
+
+ const { hostname, uptime } = require("os");
+  Amarok.addCMD(
+     {     pattern: "list",
+           isOwner: false,
+           type: "user",
+    },
+   async(conn,match) => {
+ 
+  let sections = [
+     {  
+    title: "ᴀᴍᴀʀᴏᴋ ᴍᴇɴᴜ",
+    rows: [
+         {title: "「 ᴅᴏᴡɴʟᴏᴀᴅs 」", rowld: 'selfoce', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+         {title: "「 ɢʀᴏᴜᴘ ᴍᴇɴᴜ 」", rowld: 'groups', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+         {title: "「 ʟɪsᴛ ᴍᴇɴᴜ 」", rowld: 'list', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+         {title: "「 ɢᴀᴍᴇ ᴍᴡɴᴜ 」", rowld: 'games', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`}
+    ]
+ },{
+     title: "ɴᴏᴛ ᴄᴏᴍᴍᴀɴᴅs",
+     rows: [
+           {title: "「 ᴄᴏɴᴠᴇʀᴛᴇʀ 」", rowld: 'vectors', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+           {title: "「 ᴜꜱᴇʀ ᴍᴇɴᴜ 」", rowld: 'users', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+           {title: "「 ᴛᴏᴏʟꜱ ᴍᴇɴᴜ 」", rowld: 'toolz', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+           {title: "「 ꜱᴇᴀʀᴄʜ ᴍᴇɴᴜ 」", rowld: 'searchz', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`},
+           {title: "「 ᴏᴡɴᴇʀ ᴍᴇɴᴜ 」", rowld: 'coms', description: `ғᴇᴀᴛᴜʀᴇᴅ ʙʏ ᴀᴍᴀʀᴏᴋ`}
+       ]
+   },
+  ]
+     let [date, time] = new Date()
+        .toLocaleString("en-IN", { timeZone: "Africa/Johannesburg" })
+        .split(",");
+ const listMessage = {
+    text:`*╭───────────㋰*
+*│╭──[ ${config.BOT_NAME} ]──㋰*
+*││ USER : ${conn.data.pushName}*
+*││ VERSION : ${pjson../package.json'}
+*│╰──㋰*
+*╰───────────㋰*`
+    buttonText: "ꜱᴇʟᴇᴄᴛ ϻᴇnu:)",
+sections 
+}
+return await conn.sendMessage(conn.chatld, listMessage, { react: { text: "🫂", key: conn.key,
+  quoted: conn.data
+ }})
+});
