8cff43c35c11a3ac5e064ffb46fa2882507b22f6
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-11 10:53:08 +0200

    Update close.js

--- plugins/close.js
+++ plugins/close.js
@@ -1,7 +1,7 @@
    const Amarok = require('../lib/events');
   
    Amarok.addCMD({ pattern: 'gclose', isOwner: true, type: 'groups', },
-  async(conn.match) => {
+  async(conn,match) => {
 
  await conn.groupSettingUpdate(conn.chatld, 'announcement');
  }
