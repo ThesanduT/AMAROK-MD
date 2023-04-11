71baa9eecbcb393de5b6da426da7a1e2d9a0879d
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-04-11 08:22:49 +0200

    Create _help.js

---
+++ plugins/_help.js
@@ -0,0 +1,39 @@
+const Amarok = require('../lib/events');
+const Config = require('../config');
+const pjsom = require('../package.json');
+
+Amarok.addCMD({
+	pattern: 'help',
+	desc: '',
+	isOwner: false,
+	dontAddCommandList: true 
+	}, 
+	async (conn, match) => {
+	
+  var CMD_HELP = `
+
+*◯──[ HEROKU MENU ]──◯*
+
+╭⛒ *USER*: ${conn.data.pushName}
+├⛒ *MODE*: ${Config.WORKTYPE}
+╰⛒ *VERSION*: ${pjson.version}						await conn.sendMessage(conn.chatId, { 
+				
+*Dyno*
+*setsudo*
+*getsudo*
+*delsudo*
+*getvar*
+*setvar*
+*shutdown*
+*restart*
+*allvar*
+*update*
+*update now*
+`
+
+   await conn.sendMessage(conn.chatld, {
+	                                                           image: { url: 'https://i.ibb.co/yskjc2Q/0e24566a0b06.jpg', },
+							           caption: CMD_HELP.trim(),							           footer: `amarok`,
+							           caption: 'amarok',
+   )}
+ );
